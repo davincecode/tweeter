73cdb39e2edc84d10d8d53677a88012933ddec75
davincecode <hyperscript.ca@gmail.com>
2022-03-02 08:41:46 +0000

    Update toggle menu button using jQuery

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -7,8 +7,10 @@ $(() => {
     charRemain = maxlength - charCount;
     $(".counter").text(charRemain);
   });
-});
 
-$(() => {
   timeago.render(document.querySelectorAll(".need_to_be_rendered"));
+
+  $("#toggle-tweet").click(function () {
+    $("#toggle-content").toggle();
+  });
 });
