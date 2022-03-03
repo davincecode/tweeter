bf14e5ad48d9ba911a9339480ba9bef11174aa93
davincecode <hyperscript.ca@gmail.com>
2022-03-03 00:45:33 +0000

    Update and remove comments

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -8,9 +8,15 @@ $(() => {
   $.post("/tweets", function (event) {
     event.preventDefault();
 
-    const elements = convertToHTML(tweets);
-    $("#tweetLoad").html(elements);
+    let form = $("form").serialize();
+    $("#tweetLoad").append(form);
   });
+
+  // $("form").on("submit", function (event) {
+  //   event.preventDefault();
+
+  //   let form = $("form").serialize();
+  //   $("#tweetLoad").append(form);
 
   // $.ajax({
   //   type: "POST",
@@ -23,45 +29,45 @@ $(() => {
   ////////////////////////////////////////////////////////////////
 
   // GET TWEETS
-  // const renderTweets = function (tweets) {
-  //   // loops through tweets
-  //   // calls createTweetElement for each tweet
-  //   // takes return value and appends it to the tweets container
+  const renderTweets = function (tweets) {
+    // loops through tweets
+    // calls createTweetElement for each tweet
+    // takes return value and appends it to the tweets container
 
-  //   for (const tweet of tweets) {
-  //     let $tweet = createTweetElement(tweet);
-  //     $(`.tweet-post`).append($tweet);
-  //   }
-  // };
+    for (const tweet of tweets) {
+      let $tweet = createTweetElement(tweet);
+      $(`.tweet-post`).append($tweet);
+    }
+  };
 
-  // const createTweetElement = function (tweet) {
-  //   let $tweet = $(`<div class="tweet-container">
-  //       <section class="tweet-author">
-  //         <div class="auth-info">
-  //           <img src="${tweet.user.avatars}" width="50px">
-  //           <span class="auth-name">${tweet.user.name}</span>
-  //         </div>
-  //         <div class="auth-username">
-  //           <p>${tweet.user.handle}</p>
-  //         </div>
-  //       </section>
-  //       <section class="article-tweet">
-  //         <p>${tweet.content.text}</p>
-  //       </section>
-  //       <section class="tweet-info">
-  //         <span class="need_to_be_rendered" datetime="${tweet.user.created_at}"></span>
-  //         <aside>
-  //           <a class="tweetlinks"><i class="fa-solid fa-flag"></i></a>
-  //           <a class="tweetlinks"><i class="fa-solid fa-retweet"></i></a>
-  //           <a class="tweetlinks"><i class="fa-solid fa-heart"></i></a>
-  //         </aside>
-  //       </section>
-  //     </div>`);
+  const createTweetElement = function (tweet) {
+    let $tweet = $(`<div class="tweet-container">
+        <section class="tweet-author">
+          <div class="auth-info">
+            <img src="${tweet.user.avatars}" width="50px">
+            <span class="auth-name">${tweet.user.name}</span>
+          </div>
+          <div class="auth-username">
+            <p>${tweet.user.handle}</p>
+          </div>
+        </section>
+        <section class="article-tweet">
+          <p>${tweet.content.text}</p>
+        </section>
+        <section class="tweet-info">
+          <span class="need_to_be_rendered" datetime="${tweet.user.created_at}"></span>
+          <aside>
+            <a class="tweetlinks"><i class="fa-solid fa-flag"></i></a>
+            <a class="tweetlinks"><i class="fa-solid fa-retweet"></i></a>
+            <a class="tweetlinks"><i class="fa-solid fa-heart"></i></a>
+          </aside>
+        </section>
+      </div>`);
 
-  //   return $tweet;
-  // };
+    return $tweet;
+  };
 
-  // renderTweets(data);
+  renderTweets(data);
 
   //GET
   // const loadtweets = function () {
