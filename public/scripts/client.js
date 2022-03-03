d6b25d7f2b54cc7ae654598c63711d29b8e0b6dd
davincecode <hyperscript.ca@gmail.com>
2022-03-03 06:01:05 +0000

    Update Post request working

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,13 +3,36 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-
-//$(() => {});
-
 $(() => {
   // GET request
-  const $container = $("div.container");
-  const $textTweet = $(".tweetPost");
+  var $container = $("article.container");
+  var $appendPost = $("#appendPost");
+
+  function addTweet(item) {
+    $container.prepend(`
+        <div class="tweet-container">
+          <section class="tweet-author">
+            <div class="auth-info">
+              <img src="${item.user.avatars}" width="50px">
+              <span class="auth-name">${item.user.name}</span>
+            </div>
+            <div class="auth-username">
+              <p>${item.user.handle}</p>
+            </div>
+          </section>
+          <section class="article-tweet">
+              <p>${item.content.text}</p>
+          </section>
+          <section class="tweet-info">
+              <span class="need_to_be_rendered" datetime="${item.user.created_at}"></span>
+            <aside>
+              <a class="tweetlinks"><i class="fa-solid fa-flag"></i></a>
+              <a class="tweetlinks"><i class="fa-solid fa-retweet"></i></a>
+              <a class="tweetlinks"><i class="fa-solid fa-heart"></i></a>
+            </aside>
+          </section>
+        </div>`);
+  }
 
   $.ajax({
     type: "GET",
@@ -17,30 +40,7 @@ $(() => {
     dataType: "json",
     success: function (data) {
       $.each(data, function (i, item) {
-        $container.prepend(`
-        <div class="tweet-container">
-          <section class="tweet-author">
-          <div class="auth-info">
-            <img src="${item.user.avatars}" width="50px">
-            <span class="auth-name">${item.user.name}</span>
-          </div>
-          <div class="auth-username">
-            <p>${item.user.handle}</p>
-          </div>
-        </section>
-        <section class="article-tweet">
-          <p>${item.content.text}</p>
-        </section>
-        <section class="tweet-info">
-          <span class="need_to_be_rendered" datetime="${item.user.created_at}"></span>
-          <aside>
-            <a class="tweetlinks"><i class="fa-solid fa-flag"></i></a>
-            <a class="tweetlinks"><i class="fa-solid fa-retweet"></i></a>
-            <a class="tweetlinks"><i class="fa-solid fa-heart"></i></a>
-          </aside>
-        </section>
-        </div>
-        `);
+        addTweet(item);
       });
     },
     error: function () {
@@ -50,35 +50,17 @@ $(() => {
 
   //POST request
   $("#tweet-btn").on("click", function () {
+    const update = { appendPost: $appendPost.val() };
+
     $.ajax({
       type: "POST",
       url: "/tweets",
-      data: $textTweet.val(),
-      success: function (newData) {
-        $container.prepend(`
-        <div class="tweet-container">
-          <section class="tweet-author">
-          <div class="auth-info">
-            <img src="${newData.user.avatars}" width="50px">
-            <span class="auth-name">${newData.user.name}</span>
-          </div>
-          <div class="auth-username">
-            <p>${newData.user.handle}</p>
-          </div>
-        </section>
-        <section class="article-tweet">
-          <p>${newData.content.text}</p>
-        </section>
-        <section class="tweet-info">
-          <span class="need_to_be_rendered" datetime="${newData.user.created_at}"></span>
-          <aside>
-            <a class="tweetlinks"><i class="fa-solid fa-flag"></i></a>
-            <a class="tweetlinks"><i class="fa-solid fa-retweet"></i></a>
-            <a class="tweetlinks"><i class="fa-solid fa-heart"></i></a>
-          </aside>
-        </section>
-        </div>
-        `);
+      data: update,
+      success: function (newItem) {
+        addTweet(newItem);
+      },
+      error: function () {
+        alert("Error loading tweet");
       },
     });
   });
