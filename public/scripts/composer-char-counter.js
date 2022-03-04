c649f2a760585b56998b93f4de153fa25675d253
davincecode <hyperscript.ca@gmail.com>
2022-03-04 06:29:24 +0000

    Update max character warning

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -6,7 +6,8 @@ $(() => {
 
     if (charCount > 140) {
       counter.css("color", "#ff0000");
-      return $("#error-box").prepend("<p>Max character reached</p>");
+      $("#tweet-it").prop("disabled", true);
+      return $("#error-box").prepend("<small>Max character reached!</small>");
     }
     if (!(charCount > 140)) {
       return counter.css("color", "#545454");
