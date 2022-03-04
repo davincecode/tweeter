a13da7701d0ce2632f6e9f278ab5e9bd01e05fea
davincecode <hyperscript.ca@gmail.com>
2022-03-04 04:51:10 +0000

    Update responsive layout done

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,15 @@
 $(() => {
-  $("textarea").on("keyup", function () {
-    let charCount = $("textarea").val().length;
-    let counter = $(".counter");
+  $("#appendPost").on("keyup", function () {
+    let charCount = $("#appendPost").val().length;
+    let counter = $("#counter");
     counter.text(140 - charCount);
-    charCount > 140
-      ? counter.css("color", "ff0000")
-      : counter.css("color", "#545454");
+
+    if (charCount > 140) {
+      counter.css("color", "#ff0000");
+      return `Max character reached.`;
+    }
+    if (!(charCount > 140)) {
+      return counter.css("color", "#545454");
+    }
   });
 });
